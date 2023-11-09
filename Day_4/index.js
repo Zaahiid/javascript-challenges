560d87b52ad9ce196153189cddee264cf5de7baf
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-09 11:38:38 +0530

    Day 4, Challenge 9

--- Day_4/index.js
+++ Day_4/index.js
@@ -4,10 +4,24 @@ function removeSpaces(str) {
     return str.replace(/\s/g, '');
   }
   
-  // Example usage:
+  //Example Usage:
   const stringWithSpaces = "This is an example string with spaces";
   console.log('String with spaces:', stringWithSpaces);
   
   const stringWithoutSpaces = removeSpaces(stringWithSpaces);
   console.log('String without spaces:', stringWithoutSpaces);
+  
+
+//9. Return a Boolean if a number is divisible by 10
+
+function isDivisibleByTen(number) {
+    return number % 10 === 0;
+  }
+  
+  // Example usage:
+  const num1 = 30;
+  console.log(`${num1} is divisible by 10: ${isDivisibleByTen(num1)}`);
+  
+  const num2 = 17;
+  console.log(`${num2} is divisible by 10: ${isDivisibleByTen(num2)}`);
   
