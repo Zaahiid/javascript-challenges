d072fa8db03a8518dc2e8758639767e42c23003b
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-10 11:54:12 +0530

    Day 5

--- Day_5/index.js
+++ Day_5/index.js
@@ -43,4 +43,77 @@ function twoSum(nums, target) {
   } else {
     console.log("No solution found.");
   }
+
+
+// 2. Add Two Numbers
+// Medium  27%
+
+// You are given two non-empty linked lists representing two non-negative
+// integers. The digits are stored in reverse order and each of their nodes
+// contain a single digit. Add the two numbers and return it as a linked list.
+
+// You may assume the two numbers do not contain any leading zero, except the
+// number 0 itself.
+
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+  
+/**
+ * Definition for singly-linked list.
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  
+  function addTwoNumbers(l1, l2) {
+    let dummyHead = new ListNode(0);
+    let current = dummyHead;
+    let carry = 0;
+  
+    while (l1 !== null || l2 !== null) {
+      const x = l1 !== null ? l1.val : 0;
+      const y = l2 !== null ? l2.val : 0;
+      const sum = x + y + carry;
+  
+      carry = Math.floor(sum / 10);
+      current.next = new ListNode(sum % 10);
+      current = current.next;
+  
+      if (l1 !== null) l1 = l1.next;
+      if (l2 !== null) l2 = l2.next;
+    }
+  
+    if (carry > 0) {
+      current.next = new ListNode(carry);
+    }
+  
+    return dummyHead.next;
+  }
+  
+  // Example usage:
+  const l1 = new ListNode(2);
+  l1.next = new ListNode(4);
+  l1.next.next = new ListNode(3);
+  
+  const l2 = new ListNode(5);
+  l2.next = new ListNode(6);
+  l2.next.next = new ListNode(4);
+  
+  const result2 = addTwoNumbers(l1, l2);
+  
+  // Print the result
+  let current = result2;
+  while (current !== null) {
+    console.log(current.val);
+    current = current.next;
+  }
   
