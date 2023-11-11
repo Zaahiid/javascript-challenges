82b2555a53b4c9a602bb336148d64830ba71b84c
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-11 11:44:13 +0530

    Day 6

--- Day_6/index.js
+++ Day_6/index.js
@@ -40,3 +40,68 @@ function lengthOfLongestSubstring(s) {
   console.log(lengthOfLongestSubstring("pwwkew"));   // Output: 3
   console.log(lengthOfLongestSubstring("dvdf"));     // Output: 3
   
+
+  // 4. Median of Two Sorted Arrays
+// Hard 22%
+
+// There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+// Find the median of the two sorted arrays. The overall run time complexity
+// should be O(log (m+n)).
+
+// Example 1:
+
+// nums1 = [1, 3]
+// nums2 = [2]
+
+// The median is 2.0
+
+// Example 2:
+
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+
+// The median is (2 + 3)/2 = 2.5
+
+function findMedianSortedArrays(nums1, nums2) {
+    if (nums1.length > nums2.length) {
+      [nums1, nums2] = [nums2, nums1]; // Ensure nums1 is the shorter array
+    }
+  
+    const x = nums1.length;
+    const y = nums2.length;
+    const totalLength = x + y;
+  
+    let low = 0;
+    let high = x;
+  
+    while (low <= high) {
+      const partitionX = Math.floor((low + high) / 2);
+      const partitionY = Math.floor((totalLength + 1) / 2) - partitionX;
+  
+      const maxX = partitionX === 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1];
+      const minX = partitionX === x ? Number.POSITIVE_INFINITY : nums1[partitionX];
+      
+      const maxY = partitionY === 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1];
+      const minY = partitionY === y ? Number.POSITIVE_INFINITY : nums2[partitionY];
+  
+      if (maxX <= minY && maxY <= minX) {
+        if (totalLength % 2 === 0) {
+          return (Math.max(maxX, maxY) + Math.min(minX, minY)) / 2;
+        } else {
+          return Math.max(maxX, maxY);
+        }
+      } else if (maxX > minY) {
+        high = partitionX - 1;
+      } else {
+        low = partitionX + 1;
+      }
+    }
+  
+    throw new Error("Input arrays are not sorted.");
+  }
+  
+  // Examples:
+  console.log(findMedianSortedArrays([1, 3], [2]));      // Output: 2.0
+  console.log(findMedianSortedArrays([1, 2], [3, 4]));   // Output: 2.5
+  
