c4c16d3ecb24f0b8652d91ca06264485e7c99f1d
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-22 23:55:55 +0530

    Day 11

--- Day_11/index.js
+++ Day_11/index.js
@@ -24,4 +24,20 @@ function longestCommonPrefix(strs) {
   // Examples:
   console.log(longestCommonPrefix(['flower', 'flow', 'flight']));  // Output: 'fl'
   console.log(longestCommonPrefix(['dog', 'racecar', 'car']));      // Output: ''
+
+  // 15. 3Sum
+// Medium  21%
+
+// Given an array S of n integers, are there elements a, b, c in S such that a +
+// b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+// Note: The solution set must not contain duplicate triplets.
+
+// For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+// A solution set is:
+// [
+//   [-1, 0, 1],
+//   [-1, -1, 2]
+// ]
   
