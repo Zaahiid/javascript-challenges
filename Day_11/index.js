ad797a665eaf445cb92219dd2b8f18500a2d6c1d
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-22 23:56:56 +0530

    Day 11

--- Day_11/index.js
+++ Day_11/index.js
@@ -40,4 +40,45 @@ function longestCommonPrefix(strs) {
 //   [-1, 0, 1],
 //   [-1, -1, 2]
 // ]
+
+
+function threeSum(nums) {
+    const result = [];
+  
+    nums.sort((a, b) => a - b);
+  
+    for (let i = 0; i < nums.length - 2; i++) {
+      if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        const target = -nums[i];
+  
+        while (left < right) {
+          const sum = nums[left] + nums[right];
+  
+          if (sum === target) {
+            result.push([nums[i], nums[left], nums[right]]);
+            
+            while (left < right && nums[left] === nums[left + 1]) left++;
+            while (left < right && nums[right] === nums[right - 1]) right--;
+  
+            left++;
+            right--;
+          } else if (sum < target) {
+            left++;
+          } else {
+            right--;
+          }
+        }
+      }
+    }
+  
+    return result;
+  }
+  
+  // Example:
+  const nums = [-1, 0, 1, 2, -1, -4];
+  console.log(threeSum(nums));
+  // Output: [[-1, -1, 2], [-1, 0, 1]]
+  
   
