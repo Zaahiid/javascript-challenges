acd5fce5a0d1155098c3109bfe6b2eba020fa4db
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-23 23:45:05 +0530

    Day 14

--- Day_12/index.js
+++ Day_12/index.js
@@ -42,3 +42,60 @@ function threeSumClosest(nums, target) {
   const target = 1;
   console.log(threeSumClosest(nums, target));  // Output: 2
   
+
+  // 17. Letter Combinations of a Phone Number
+// Medium   35%
+
+// Given a digit string, return all possible letter combinations that the number
+// could represent.
+
+// A mapping of digit to letters (just like on the telephone buttons) is given
+// below.
+
+// Input:Digit string "23"
+// Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+
+// Note: Although the above answer is in lexicographical order, your answer
+// could be in any order you want.
+
+function letterCombinations(digits) {
+    if (digits.length === 0) {
+      return [];
+    }
+  
+    const digitMap = {
+      '2': 'abc',
+      '3': 'def',
+      '4': 'ghi',
+      '5': 'jkl',
+      '6': 'mno',
+      '7': 'pqrs',
+      '8': 'tuv',
+      '9': 'wxyz',
+    };
+  
+    const result = [];
+  
+    function backtrack(index, current) {
+      if (index === digits.length) {
+        result.push(current);
+        return;
+      }
+  
+      const letters = digitMap[digits[index]];
+  
+      for (let i = 0; i < letters.length; i++) {
+        backtrack(index + 1, current + letters[i]);
+      }
+    }
+  
+    backtrack(0, '');
+  
+    return result;
+  }
+  
+  // Example:
+  const digits = '23';
+  console.log(letterCombinations(digits));
+  // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+  
