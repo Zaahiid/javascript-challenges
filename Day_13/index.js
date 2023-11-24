a5f3e003135c7a4a048638a26bb15b1a8a661459
Zaahiid <zahidrsheikh01@gmail.com>
2023-11-24 23:57:13 +0530

    Day 13

--- Day_13/index.js
+++ Day_13/index.js
@@ -61,3 +61,75 @@ function fourSum(nums, target) {
   console.log(fourSum(nums, target));
   // Output: [[-2, -1, 1, 2], [-2, 0, 0, 2], [-1, 0, 0, 1]]
   
+
+  // 19. Remove Nth Node From End of List
+// Medium   33%
+
+// Given a linked list, remove the nth node from the end of list and return its
+// head.
+
+// For example,
+
+// Given linked list: 1->2->3->4->5, and n = 2.
+
+// After removing the second node from the end, the linked list becomes
+// 1->2->3->5.
+
+// Note:
+// Given n will always be valid.
+//   Try to do this in one pass.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+  }
+  
+  function removeNthFromEnd(head, n) {
+    const dummy = new ListNode(0);
+    dummy.next = head;
+  
+    let fast = dummy;
+    let slow = dummy;
+  
+    // Move fast pointer n nodes ahead
+    for (let i = 0; i <= n; i++) {
+      fast = fast.next;
+    }
+  
+    // Move both pointers until fast reaches the end
+    while (fast !== null) {
+      fast = fast.next;
+      slow = slow.next;
+    }
+  
+    // Remove the nth node from the end
+    slow.next = slow.next.next;
+  
+    return dummy.next;
+  }
+  
+  // Example:
+  const linkedList = new ListNode(1);
+  linkedList.next = new ListNode(2);
+  linkedList.next.next = new ListNode(3);
+  linkedList.next.next.next = new ListNode(4);
+  linkedList.next.next.next.next = new ListNode(5);
+  
+  const n = 2;
+  const updatedList = removeNthFromEnd(linkedList, n);
+  
+  // Print the updated list
+  let current = updatedList;
+  while (current !== null) {
+    console.log(current.val);
+    current = current.next;
+  }
+  
